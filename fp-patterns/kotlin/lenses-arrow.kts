2f7abbad1d84f516ec9cd598234e66b42e708ea6
Richard Ashworth <richardashworth575@gmail.com>
2019-03-26 22:12:26 +0000

    lenses with arrow-kt lib

---
+++ fp-patterns/kotlin/lenses-arrow.kts
@@ -0,0 +1,25 @@
+@file:Include("adts.kts")
+
+@file:DependsOn("io.arrow-kt:arrow-optics:0.9.0")
+import arrow.optics.*
+
+val developerPersonLens: Lens<Employee.Developer, Person> = Lens(
+    get = { dev -> dev.who },
+    set = { dev, person -> dev.copy(who = person) }
+)
+
+val personAddressLens: Lens<Person, Address> = Lens(
+    get = { person -> person.address },
+    set = { person, a -> person.copy(address = a) }
+)
+
+val addressPostcodeLens: Lens<Address, String> = Lens(
+    get = { address -> address.postcode },
+    set = { address, pc -> address.copy(postcode = pc) }
+)
+
+val developerAddressLens = developerPersonLens compose personAddressLens
+val developerPostcodeLens = developerAddressLens compose addressPostcodeLens
+
+println(developerPostcodeLens.get(dev))
+println(developerPostcodeLens.set(dev, "NEW POSTCODE"))
