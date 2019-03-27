2f8ad674093e700987a472d320de033e51062dd0
Richard Ashworth <richardashworth575@gmail.com>
2019-03-27 11:07:40 +0000

    whitespace

--- fp-patterns/kotlin/monoid-arrow.kts
+++ fp-patterns/kotlin/monoid-arrow.kts
@@ -13,8 +13,7 @@ sealed class Currency(val name: String, val fxRate: Double) {
 
 data class Payment(val amount: Double, val ccy: Currency)
 
-
-val PaymentMonoid: Monoid<Payment> = object:Monoid<Payment> {
+val PaymentMonoid: Monoid<Payment> = object: Monoid<Payment> {
   override fun empty(): Payment = Payment(0.0, Currency.GBP)
   override fun Payment.combine(b: Payment): Payment =
     Payment(amount * ccy.fxRate + b.amount * b.ccy.fxRate, Currency.GBP)
