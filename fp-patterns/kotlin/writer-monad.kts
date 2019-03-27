1d9b55bd37bf6fe98d19b5c9123f1fa40d622464
Richard Ashworth <richardashworth575@gmail.com>
2019-03-27 12:54:07 +0000

    script

--- fp-patterns/kotlin/writer-monad.kts
+++ fp-patterns/kotlin/writer-monad.kts
@@ -38,14 +38,10 @@ class Writer<T : Any> private constructor(val value: T, s: String) {
 }
 
 fun root(d: Double) = Writer.unit(sqrt(d), "Took square root")
-
 fun addOne(d: Double) = Writer.unit(d + 1.0, "Added one")
-
 fun half(d: Double) = Writer.unit(d / 2.0, "Divided by two")
 
-fun main(args: Array<String>) {
-  val iv = Writer.unit(5.0, "Initial value")
-  val fv = iv.bind(::root).bind(::addOne).bind(::half)
-  println("The Golden Ratio is ${fv.value}")
-  println("\nThis was derived as follows:-\n${fv.log}")
-}
+val iv = Writer.unit(5.0, "Initial value")
+val fv = iv.bind(::root).bind(::addOne).bind(::half)
+println("The Golden Ratio is ${fv.value}")
+println("\nThis was derived as follows:-\n${fv.log}")
